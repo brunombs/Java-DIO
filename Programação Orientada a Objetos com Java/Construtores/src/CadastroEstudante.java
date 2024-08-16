a8933c37bd86a07f593a80be5da695c9e4cc2b41
brunombs <99062708+brunombs@users.noreply.github.com>
2024-08-16 18:25:29 -0300

    Delete Programação Orientada a Objetos com Java directory

--- Programação Orientada a Objetos com Java/Construtores/src/CadastroEstudante.java
+++
@@ -1,6 +0,0 @@
-public class CadastroEstudante {
-    public static void main(String[] args) {
-        Aluno bruno = new Aluno(405, "Bruno", "123.456.789-00");
-        System.out.println(bruno.getNome() + "-" + bruno.getCpf());
-    }
-}
