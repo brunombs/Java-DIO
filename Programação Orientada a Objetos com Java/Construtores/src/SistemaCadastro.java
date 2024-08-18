45699259636dd2e6be554d53175b87d562d75eb5
brunombs <brunozs@msn.com>
2024-08-18 17:28:04 -0300

    Remove as pastas 'Dominando a Linguagem de Programação Java' e 'Programação Orientada a Objetos com Java'

--- Programação Orientada a Objetos com Java/Construtores/src/SistemaCadastro.java
+++
@@ -1,9 +0,0 @@
-public class SistemaCadastro {
-    public static void main(String[] args) {
-        Pessoa marcos = new Pessoa("123.456.789-01", "Marcos");
-
-        marcos.setEndereco("Rua das Marias");
-        System.out.println(marcos.getNome() + " " + marcos.getCpf());
-    }
-
-}
