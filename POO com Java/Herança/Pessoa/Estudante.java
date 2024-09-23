8f0b80967d6893868e7f780a24c174c867a7c658
brunombs <brunozs@msn.com>
2024-09-23 01:59:05 -0300

    adding exercise

---
+++ POO com Java/Herança/Pessoa/Estudante.java
@@ -0,0 +1,11 @@
+public class Estudante extends Pessoa{
+    private int matricula;
+
+    public int getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(int matricula) {
+        this.matricula = matricula;
+    }
+}
