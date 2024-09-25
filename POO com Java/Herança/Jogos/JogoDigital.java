eff651fbd11139f1ba30e3bebb1126a656597560
brunombs <brunozs@msn.com>
2024-09-25 01:06:25 -0300

    improving exercise

--- POO com Java/Herança/Jogos/JogoDigital.java
+++ POO com Java/Herança/Jogos/JogoDigital.java
@@ -1,38 +1,11 @@
-public class JogoDigital {
-    String titulo;
-    String genero;
-    double preco;
+public class JogoDigital extends Jogos {
     double tamanhoDownload;
-
-    public double getPreco() {
-        return preco;
-    }
 
     public double getTamanhoDownload() {
         return tamanhoDownload;
     }
 
-    public String getGenero() {
-        return genero;
-    }
-
-    public String getTitulo() {
-        return titulo;
-    }
-
-    public void setGenero(String genero) {
-        this.genero = genero;
-    }
-
-    public void setPreco(double preco) {
-        this.preco = preco;
-    }
-
     public void setTamanhoDownload(double tamanhoDownload) {
         this.tamanhoDownload = tamanhoDownload;
-    }
-
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
     }
 }
--- POO com Java/Herança/Jogos/JogoFisico.java
+++ POO com Java/Herança/Jogos/JogoFisico.java
@@ -1,32 +1,5 @@
-public class JogoFisico {
-    String titulo;
-    String genero;
-    double preco;
+public class JogoFisico extends Jogos {
     double peso;
-
-    public String getTitulo() {
-        return titulo;
-    }
-
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
-    }
-
-    public String getGenero() {
-        return genero;
-    }
-
-    public void setGenero(String genero) {
-        this.genero = genero;
-    }
-
-    public double getPreco() {
-        return preco;
-    }
-
-    public void setPreco(double preco) {
-        this.preco = preco;
-    }
 
     public double getPeso() {
         return peso;
