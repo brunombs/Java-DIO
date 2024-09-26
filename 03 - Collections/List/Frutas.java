c9775e3f7ab89b3533f38d89b54cf535b4c55c37
brunombs <brunozs@msn.com>
2024-09-26 02:17:23 -0300

    adding exercise

---
+++ 03 - Collections/List/Frutas.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+public class Frutas {
+    public static void main(String[] args) {
+        ArrayList<String> frutas = new ArrayList<>();
+        frutas.add("Banana");
+        frutas.add("Maçã");
+        frutas.add("Morango");
+        frutas.add("Abacaxi");
+        frutas.add("Uva");
+        System.out.println("Lista inicial com todas as frutas: " + frutas);
+        frutas.removeLast();
+        System.out.println("Lista com todas as frutas, exceto a última da lista anterior: " + frutas);
+        frutas.remove("Morango");
+        System.out.println("Lista inicial com exceção de duas frutas: " + frutas);
+    }
+}
